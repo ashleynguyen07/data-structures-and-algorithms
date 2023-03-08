0551eb3e16ce2247d8c54e9dd1e640b7baa7358a
ashleynguyen07 <nguyentramy.se@gmail.com>
2023-03-08 10:00:48 +0700

    login system tp bank

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tramy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Validation valid = new Validation();
+        valid.display();
+        
+        
+    }
+}
